4884d22c41bf912a71a0603d890e3bc5a098f2fb
codaengii2 <carla7039@naver.com>
2024-01-04 15:09:29 +0900

    비디오태그

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,13 @@ import {
   Spinner,
   Heading,
   Center,
+  AspectRatio,
 } from "@chakra-ui/react";
-import { ArrowForwardIcon, DownloadIcon } from "@chakra-ui/icons";
+import {
+  ArrowForwardIcon,
+  DownloadIcon,
+  ArrowBackIcon,
+} from "@chakra-ui/icons";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { getData } from "../api";
@@ -48,25 +53,37 @@ export const Home = () => {
     setDate(previousDate);
     queryClient.invalidateQueries(["apod", previousDate]); //캐시의 apod 무효화
   };
+  const handleNextDay = () => {
+    const currentDate = new Date(date);
+    currentDate.setDate(currentDate.getDate() + 1);
+    const year = currentDate.getFullYear();
+    const month = ("0" + (1 + currentDate.getMonth())).slice(-2);
+    const day = ("0" + currentDate.getDate()).slice(-2);
+    const nextDate = year + "-" + month + "-" + day;
 
+    setDate(nextDate);
+    queryClient.invalidateQueries(["apod", nextDate]); //캐시의 apod 무효화
+  };
+
+  // console.log(apodData);
   const saveImage = () => {
     if (apodData && apodData.hdurl) {
       saveAs(apodData.hdurl, "image.jpg");
     }
   };
-
   return (
     <>
       <Box bg="#000" color="#fff" padding={"5"} h={"100vh"}>
+        <Heading
+          fontWeight="bold"
+          fontSize="5xl"
+          textAlign={"left"}
+          letterSpacing="-2"
+        >
+          WELCOME TO THE <br /> ASTRONOMY PICTURE OF THE DAY
+        </Heading>
+
         <Box textAlign="center" p={4}>
-          <Heading
-            fontWeight="bold"
-            fontSize="5xl"
-            textAlign={"left"}
-            letterSpacing="-2"
-          >
-            WELCOME TO THE <br /> ASTRONOMY PICTURE OF THE DAY
-          </Heading>
           <Flex
             alignItems={"center"}
             w="80vw"
@@ -85,7 +102,7 @@ export const Home = () => {
                 <Box mt={8}>
                   <Flex
                     justifyContent={"space-between"}
-                    w="68vw"
+                    w="70vw"
                     alignItems={"center"}
                   >
                     <Flex direction={"column"} align="flex-start" h="80vh">
@@ -97,18 +114,35 @@ export const Home = () => {
                       </Text>
                     </Flex>
                     <Center>
-                      <Image
-                        src={
-                          apodData.hdurl
-                            ? apodData.hdurl
-                            : "https://i.pinimg.com/originals/70/65/4f/70654fd75f2991e168ec22b75fdbd6a5.gif"
-                        }
-                        alt={apodData.title}
-                        maxW="700"
-                        maxH={"80vh"}
-                        overflow="hidden"
-                        // m={4}
-                      />
+                      {apodData.media_type === "image" ? (
+                        <Image
+                          src={apodData.url}
+                          alt={apodData.title}
+                          maxW="700"
+                          maxH={"80vh"}
+                          overflow="hidden"
+                          ml={4}
+                        />
+                      ) : apodData.media_type === "video" ? (
+                        <AspectRatio w={700} ratio={1} maxH={"80vh"}>
+                          <iframe
+                            title="video"
+                            src={apodData.url}
+                            allowFullScreen
+                          ></iframe>
+                        </AspectRatio>
+                      ) : (
+                        <Image
+                          src={
+                            "https://i.pinimg.com/originals/7b/bf/81/7bbf813292b6cd4e5835155661aea32d.gif"
+                          }
+                          alt={"no Data"}
+                          maxW="700"
+                          maxH={"80vh"}
+                          overflow="hidden"
+                          ml={4}
+                        />
+                      )}
                     </Center>
                     <Button onClick={saveImage} ml="3">
                       <DownloadIcon />
@@ -122,8 +156,28 @@ export const Home = () => {
                 onClick={handlePreviousDay}
                 colorScheme="gray"
                 variant="outline"
+                mr={"2"}
+                _hover={{
+                  bg: "#fff",
+                  "& svg": {
+                    color: "#000",
+                  },
+                }}
               >
-                <ArrowForwardIcon color={"#Fff"} />
+                <ArrowBackIcon color={"#fff"} />
+              </Button>
+              <Button
+                onClick={handleNextDay}
+                colorScheme="gray"
+                variant="outline"
+                _hover={{
+                  bg: "#fff",
+                  "& svg": {
+                    color: "#000",
+                  },
+                }}
+              >
+                <ArrowForwardIcon color={"#fff"} />
               </Button>
             </Flex>
           </Flex>
